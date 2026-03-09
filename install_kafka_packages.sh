d0353f4c5553f80147e98ed189548785cc2335c5
Kh-1976 <78970676+Kh-1976@users.noreply.github.com>
2026-03-09 20:22:53 +0300

    Update install_kafka_packages.sh
    Commit 2

--- install_kafka_packages.sh
+++ install_kafka_packages.sh
@@ -1,23 +1,28 @@
 #!/bin/bash
 
 # Получаем имя пода scheduler
-POD_NAME=$(minikube kubectl -- get pods -n airflow -l component=scheduler -o jsonpath="{.items[0].metadata.name}")
-
+SCHEDULER_NAME=$(minikube kubectl -- get pods -n airflow -l component=scheduler -o jsonpath="{.items[0].metadata.name}")
 # Получаем имя пода worker
 WORKER_POD=$(minikube kubectl -- get pods -n airflow -l component=worker -o jsonpath="{.items[0].metadata.name}")
+# Получаем имя пода dag-processor
+DAG_POD=$(minikube kubectl -- get pods -n airflow -l component=dag-processor -o jsonpath="{.items[0].metadata.name}")
+# Получаем имя пода triggerer
+TRIGGER_POD=$(minikube kubectl -- get pods -n airflow -l component=triggerer -o jsonpath="{.items[0].metadata.name}")
 
-echo "Installing packages in scheduler pod: $POD_NAME"
+echo "Installing packages in scheduler pod: $SCHEDULER_NAME"
 # Устанавливаем пакеты в scheduler
-minikube kubectl -- exec -it $POD_NAME -n airflow -- /bin/bash -c "pip install confluent-kafka kafka-python"
+minikube kubectl -- exec -it $SCHEDULER_NAME -n airflow -- /bin/bash -c "pip install confluent-kafka kafka-python"
 
 echo "Installing packages in worker pod: $WORKER_POD"
 # Устанавливаем пакеты в worker
 minikube kubectl -- exec -it $WORKER_POD -n airflow -- /bin/bash -c "pip install confluent-kafka kafka-python"
 
-echo "Verifying installation in scheduler:"
-minikube kubectl -- exec -it $POD_NAME -n airflow -- pip list | grep kafka
+echo "Installing packages in worker pod: $DAG_POD"
+# Устанавливаем пакеты в worker
+minikube kubectl -- exec -it $DAG_POD -n airflow -- /bin/bash -c "pip install confluent-kafka kafka-python"
 
-echo "Verification in worker:"
-minikube kubectl -- exec -it $WORKER_POD -n airflow -- pip list | grep kafka
+echo "Installing packages in worker pod: $TRIGGER_POD"
+# Устанавливаем пакеты в worker
+minikube kubectl -- exec -it $TRIGGER_POD -n airflow -- /bin/bash -c "pip install confluent-kafka kafka-python"
 
 echo "All packages installed successfully!"
